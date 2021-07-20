d350a2e410c7f512237e5e3669b69d9e9fc90a69
TanveshRongala <tanveshrongala@gmail.com>
2021-07-20 18:24:15 +0530

    TicTacToeGame

---
+++ src/TicTacToe/GameOperations.java
@@ -0,0 +1,15 @@
+package TicTacToe;
+
+public class GameOperations {
+	public char[] gameBoard= new char[10];
+	
+	public GameOperations() {
+		
+		for(int i=0;i<gameBoard.length;i++) {
+			System.out.print(gameBoard[i]);
+			
+		}
+		
+		
+	}
+}
